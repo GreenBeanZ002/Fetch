6ed5e895b155fdc8b69f910a37d072e0224fbaf3
GreenBeanZ002 <cjpgreen@outlook.com>
2024-12-03 15:44:07 +0000

    Added Getters/Setters

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,6 +4,19 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField] private float moveSpeed = 50f;
+    [SerializeField] private float rotationAmt = 15f;
+    [SerializeField] private int Health = 10;
+
+    //Getters and Setters
+    public float GetMoveSpeed() { return moveSpeed; }
+    public void SetMoveSpeed(float moveSpeed) { moveSpeed = moveSpeed; }
+    public float GetRotationAmt() { return rotationAmt; }
+    public void SetRotationAmt(float rotationAmt) { rotationAmt = rotationAmt; }
+    public int GetHealth() { return Health; }
+    public void SetHealth(int Health) { Health = Health; }
+
+
     // Start is called before the first frame update
     void Start()
     {

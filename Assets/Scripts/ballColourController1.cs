e865b65ac4bd568408002fe0fb87021cd1bf9410
GreenBeanZ002 <cjpgreen@outlook.com>
2025-01-16 18:50:46 +0000

    Rewrote ball controller

--- Assets/Scripts/ballColourController1.cs
+++ Assets/Scripts/ballColourController1.cs
@@ -2,17 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class bedColourController : MonoBehaviour
+public class ballColourController : MonoBehaviour
 {
-    public int bedColor = 1; 
-    [SerializeField] private GameObject bed;
-    [SerializeField] private Sprite bed1;
-    [SerializeField] private Sprite bed2;
-    [SerializeField] private Sprite bed3;
-    [SerializeField] private Sprite bed4;
+    public int ballColor = 1; 
+    [SerializeField] private GameObject ball;
+    [SerializeField] private Sprite ball1;
+    [SerializeField] private Sprite ball2;
+    [SerializeField] private Sprite ball3;
+    [SerializeField] private Sprite ball4;
 
-    public int GetBedColour () { return bedColor; }
-    public void SetBedColor (int bedAmt) { bedColor = bedAmt; }
+    public int GetBallColour () { return ballColor; }
+    public void SetBallColor (int ballAmt) { ballColor = ballAmt; }
 
     // Start is called before the first frame update
     void Start()
@@ -23,19 +23,19 @@ public class bedColourController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        switch (bedColor)
+        switch (ballColor)
         {
             case 1:
-                bed.GetComponent<SpriteRenderer>().sprite = bed1; 
+                ball.GetComponent<SpriteRenderer>().sprite = ball1; 
                 break;
             case 2:
-                bed.GetComponent<SpriteRenderer>().sprite = bed2;
+                ball.GetComponent<SpriteRenderer>().sprite = ball2;
                 break;
             case 3:
-                bed.GetComponent<SpriteRenderer>().sprite = bed3;
+                ball.GetComponent<SpriteRenderer>().sprite = ball3;
                 break; 
             case 4:
-                bed.GetComponent<SpriteRenderer>().sprite = bed4;
+                ball.GetComponent<SpriteRenderer>().sprite = ball4;
                 break; 
         }
     }

a04792e40f598cc5ea80e23373de751c0c6beba4
GreenBeanZ002 <cjpgreen@outlook.com>
2025-01-20 11:50:51 +0000

    Navigating between menus

--- Assets/Scripts/ButtonScripts.cs
+++ Assets/Scripts/ButtonScripts.cs
@@ -6,7 +6,14 @@ using UnityEngine.SceneManagement;
 public class ButtonScripts : MonoBehaviour
 {
     [SerializeField] private Canvas Settings;
+    [SerializeField] private Canvas HUD;
     [SerializeField] private Canvas Help;
+    private int BreedIncrement;
+    private int BallIncrement;
+    private int BedIncrement;
+    public dogSpriteController dogSpriteController;
+    public bedColourController BedColourController;
+    public ballColourController BallColourController;
     public void replayGame()
     {
         SceneManager.LoadSceneAsync(1);
@@ -18,19 +25,24 @@ public class ButtonScripts : MonoBehaviour
     public void openSettings()
     {
         Settings.enabled = true;
+        HUD.enabled = false;
+
     }
     public void closeSettings() 
     {
         Settings.enabled = false;
+        HUD.enabled = true; 
     }
 
     public void openHelp()
     {
         Help.enabled = true;
+        HUD.enabled = false;
     }
     public void closeHelp()
     {
         Help.enabled = false;
+        HUD.enabled = true;
     }
     public void ChooseBreed()
     {
